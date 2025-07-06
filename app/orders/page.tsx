bae2770703cb5ede465370004777e0e2c3e5e5cc
Raphael Andrei Abad <rabad@gbox.adnu.edu.ph>
2025-07-07 01:38:11 +0800

    Update page.tsx

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { useState, useEffect } from "react"
 import { useSearchParams } from "next/navigation"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
 import { supabase } from "@/lib/supabase-auth"
 
 interface Order {
