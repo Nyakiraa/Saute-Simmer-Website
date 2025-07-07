c6cc3122af600ef476f628070f6f13ec633a6913
Raphael Andrei Abad <rabad@gbox.adnu.edu.ph>
2025-07-07 21:12:16 +0800

    Update page.tsx

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
 
 interface MealSet {
   id: number
